4e12225181af07f92c0c32654879e71cc5fce3fa
Nishon Tandukar <nishon.tan@gmail.com>
2019-07-03 15:47:49 +0545

    Adds login creds to .gitignore

--- app/src/main/java/np/com/naxa/staffattendance/attedancedashboard/AttedanceLocalSource.kt
+++ app/src/main/java/np/com/naxa/staffattendance/attedancedashboard/AttedanceLocalSource.kt
@@ -14,7 +14,10 @@ class AttedanceLocalSource {
 
     fun getAttendanceForDate(date: String?, new_attendance: AttendanceResponse, team_id: String?): AttendanceResponse? {
         val oldAttendance = attedanceDao.getAttedanceByDate(team_id, date)
-        if (oldAttendance.presentStaffIds != null && oldAttendance.presentStaffIds.size > 0) {
+        val hasAttedanceStarted = oldAttendance.presentStaffIds != null && oldAttendance.presentStaffIds.size > 0
+        val attendanceNotTaken = !oldAttendance.presentStaffIds.contains(new_attendance.presentStaffIds[0].toString())//we always add one staff at a time so [0] should be safe
+
+        if (hasAttedanceStarted && attendanceNotTaken) {
             oldAttendance.setStaffs(oldAttendance.presentStaffIds + new_attendance.presentStaffIds)
             return oldAttendance
         }
